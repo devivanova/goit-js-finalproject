7bb8c5129cc8f07df0426a44509c62caacd82353
devivanova <119169640+devivanova@users.noreply.github.com>
2023-10-19 10:54:45 +0200

    added task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryImage({ original, preview, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+}
+
+function selectGalleryImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img class="gallery__image-big" src="${event.target.dataset.source}">`,
+    {
+      onShow: () => window.addEventListener("keydown", onKeydownEsc),
+      onClose: () => window.removeEventListener("keydown", onKeydownEsc),
+    },
+  );
+
+  const onKeydownEsc = event => {
+    if (event.code === "Escape") instance.close();
+  };
+
+  instance.show();
+}
+
+const galleryCardsSet = galleryItems.map(createGalleryImage).join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryCardsSet);
+galleryList.addEventListener("click", selectGalleryImage);
